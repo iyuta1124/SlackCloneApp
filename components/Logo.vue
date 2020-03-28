4ce950ca1bb47df2a3b0c9f596e59c15b8e7fa8e
iyuta1124 <iyuta1124@gmail.com>
2020-03-28 22:00:45 +0900

    w245から250

--- components/Logo.vue
+++ components/Logo.vue
@@ -7,6 +7,16 @@
   </div>
 </template>
 
+<style>
+.VueToNuxtLogo {
+  display: inline-block;
+  animation: turn 2s linear forwards 1s;
+  transform: rotateX(180deg);
+  position: relative;
+  overflow: hidden;
+  height: 180px;
+  width: 250px;
+}
 
 .Triangle {
   position: absolute;
